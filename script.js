e0358d98ea4ff8139a815c83c4e174586244e0c7
Mikael Daluz <Mikael.daluz@uconn.edu>
2025-05-12 03:25:46 -0400

    html fix

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const codeMap = {
-    "damn": "songs/damn.html",
-    "slap": "songs/slap.html"
-  };
-  
-  function handleCodeSubmit() {
-    const input = document.getElementById("codeInput").value.trim().toLowerCase();
-    if (codeMap[input]) {
-      window.location.href = codeMap[input];
-    } else {
-      alert("Invalid code. Try again.");
-    }
+  "song1": "songs/song1.html",
+  "song2": "songs/song2.html"
+};
+
+function handleCodeSubmit() {
+  const input = document.getElementById("codeInput").value.trim().toLowerCase().replace(/\s/g, "");
+  if (codeMap[input]) {
+    window.location.href = codeMap[input];
+  } else {
+    alert("Invalid code. Try again.");
   }
-  
+}
